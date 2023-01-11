7135ce5d7dab7a23077d12ee2ff9860856f07e5f
mike22lfc <mike22lfc@users.noreply.github.com>
2023-01-11 15:31:39 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
-  [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+  #[
+    "Beijing, CN",
+    116.39213,
+    39.90071
   ],
   [
     "ZSTU, CN",
     120.36238,
     30.31
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
   ]
 ];
